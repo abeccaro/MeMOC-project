4e7d489a9a75da385d8aeb27c12806f7ae903490
abeccaro <alex.becks@hotmail.it>
2018-02-01 21:41:53 +0100

    Made populations evaluation parallel

--- src/meta-heuristic/main.cpp
+++ src/meta-heuristic/main.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <stdexcept>
 #include <sys/time.h>
+#include <thread>
+#include <future>
 #include "../TSPData.h"
 #include "tsp_path_solution.h"
 #include "tsp_population.h"
@@ -12,52 +14,63 @@ const int POPULATION_SIZE = 1000;
 const int GENERATIONS = 500;
 const double SURVIVAL_RATIO = 0.1;
 const double MUTATION_CHANCE = 0.1;
-const int RESTARTS = 5;
+const int POPULATIONS = 5;
+
+
+/**
+ * Creates a new population, creates specified amount of generation and returns the best solution found
+ */
+tsp_path_solution evaluate_random_population(const TSPData& data) {
+	// creating first gen random population
+	tsp_population pop(data.size(), POPULATION_SIZE);
+	
+	//evaluate population
+	pop.evaluate(data.costs());
+	
+	// new generations
+	for (int i = 0; i < GENERATIONS; i++) {
+	    pop.new_generation(SURVIVAL_RATIO, MUTATION_CHANCE);
+	    pop.evaluate(data.costs());
+	}
+
+	// returning best solution
+	return pop[0];
+}
 
 
 int main (int argc, char const *argv[]) {
 	// reading data from input file
 	if (argc < 2)
-	    throw std::runtime_error("usage: ./main filename.dat");
+	    throw std::runtime_error("usage: ./main data_filename.dat");
+
 	TSPData data;
 	data.read(argv[1]);
 	
 	// setting random seed (different every second)
 	srand(time(0));
 
-        // start timer
-        struct timeval  tv1, tv2;
-        gettimeofday(&tv1, NULL);
+    // start timer
+    struct timeval  tv1, tv2;
+    gettimeofday(&tv1, NULL);
 
 	// setting best cost to INFINITE with random solution (will be overriden)
 	tsp_path_solution best(data.size());
 	
-	// starting restarts loop
+	// starting 'POPULATIONS' random populations evaluation
+	std::future<tsp_path_solution> bests[POPULATIONS];
 	std::cout << "********** START **********" << std::endl << std::endl;
-	for (int r = 0; r < RESTARTS; r++) {
-		std::cout << "Restart n. " << r + 1 << std::endl;
+	for (int i = 0; i < POPULATIONS; i++)
+		 bests[i] = std::async(std::launch::async, evaluate_random_population, data);
 
-		// creating first gen random population
-		tsp_population pop(data.size(), POPULATION_SIZE);
-		
-		//evaluate population
-		pop.evaluate(data.costs());
-		// double last = pop[0].fitness();
-		
-		// new generations
-		for (int i = 0; i < GENERATIONS; i++) {
-		    pop.new_generation(SURVIVAL_RATIO, MUTATION_CHANCE);
-		    pop.evaluate(data.costs());
-		    // if (pop[0].fitness() < last) {
-		    	// std::cout << "Gen " << i + 1 << ": " << pop[0] << std::endl;
-		    	// last = pop[0].fitness();
-			// }
-		}
+	// getting best solution from each population and storing the best found
+	for (int i = 0; i < POPULATIONS; i++) {
+		tsp_path_solution best_sol = bests[i].get();
 
-		if (pop[0].fitness() < best.fitness())
-			best = pop[0];
-		std::cout << "Best solution found: " << std::endl;
-		std::cout << pop[0] << std::endl << std::endl;
+		if (best_sol.fitness() < best.fitness())
+			best = best_sol;
+
+		std::cout << "Best solution found in population n. " << i + 1 << ": " << std::endl;
+		std::cout << best_sol << std::endl << std::endl;
 	}
 	std::cout << "*********** END ***********" << std::endl;
 
