c98cdcfdf1cdc249a81d5fb03040441cdf361299
abeccaro <alex.becks@hotmail.it>
2018-02-01 11:23:10 +0100

    Implemented random restarts

--- src/meta-heuristic/main.cpp
+++ src/meta-heuristic/main.cpp
@@ -8,9 +8,10 @@
 
 
 const int POPULATION_SIZE = 1000;
-const int GENERATIONS = 2000;
+const int GENERATIONS = 300;
 const double SURVIVAL_RATIO = 0.1;
 const double MUTATION_CHANCE = 0.1;
+const int RESTARTS = 5;
 
 
 int main (int argc, char const *argv[]) {
@@ -23,23 +24,33 @@ int main (int argc, char const *argv[]) {
 	// setting random seed (different every second)
 	srand(time(0));
 
-	// creating first gen random population
-	tsp_population pop(data.size(), POPULATION_SIZE);
-	
-	//evaluate population
-	pop.evaluate(data.costs());
+	tsp_path_solution best(data.size());
 	std::cout << "********** START **********" << std::endl;
-	std::cout << "Gen 0: " << pop[0] << std::endl;
-	double last = pop[0].fitness();
-	
-	// new generations
-	for (int i = 0; i < GENERATIONS; i++) {
-	    pop.new_generation(SURVIVAL_RATIO, MUTATION_CHANCE);
-	    pop.evaluate(data.costs());
-	    if (pop[0].fitness() < last) {
-	    	std::cout << "Gen " << i + 1 << ": " << pop[0] << std::endl;
-	    	last = pop[0].fitness();
+	for (int r = 0; r < RESTARTS; r++) {
+		std::cout << "Restart n. " << r << std::endl;
+
+		// creating first gen random population
+		tsp_population pop(data.size(), POPULATION_SIZE);
+		
+		//evaluate population
+		pop.evaluate(data.costs());
+		// double last = pop[0].fitness();
+		
+		// new generations
+		for (int i = 0; i < GENERATIONS; i++) {
+		    pop.new_generation(SURVIVAL_RATIO, MUTATION_CHANCE);
+		    pop.evaluate(data.costs());
+		    // if (pop[0].fitness() < last) {
+		    	// std::cout << "Gen " << i + 1 << ": " << pop[0] << std::endl;
+		    	// last = pop[0].fitness();
+			// }
 		}
+
+		if (pop[0].fitness() < best.fitness())
+			best = pop[0];
+		std::cout << "Best solution found: " << std::endl;
+		std::cout << pop[0] << std::endl << std::endl;
 	}
 	std::cout << "*********** END ***********" << std::endl;
+	std::cout << std::endl << "Best overall solution:" << std::endl << best << std::endl;
 }
